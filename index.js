79bb340ea0bfa23907d9eb72d37e923da29ffe37
anoop-mobikasa <anoop@mobikasa.com>
2023-12-21 12:22:26 +0530

    separate folder client

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors');
 const socketio = require('socket.io');
-const app = express();
+const http = require('http');
 const bodyParser = require('body-parser');
-
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const server = app.listen(1337, '0.0.0.0', () => {
-    console.log('Server running!')
-})
-
-const io = socketio(server)
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 io.on('connection', (socket) => {
-    console.log(`New connection: ${socket.id}`)
-
-    socket.on('message', (data) => {
-        console.log(`New message from ${socket.id}: ${data}`);
-        io.emit("notification", data);
-    })
-
-})
+  console.log(`New connection: ${socket.id}`);
+  
+  socket.on('message', (data) => {
+    console.log(`New message from ${socket.id}: ${data}`);
+    io.emit("notification", data);
+  });
+});
 
 const path = require('path');
-
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.post('/api/send-notification', (req, res) => {
-    const data = req.body; // Assuming the data is in the request body
-    console.log('Received data:', data);
-    io.emit('notification', JSON.stringify(data)); // Emit the notification event
-    res.status(200).send('Notification sent successfully');
+  const data = req.body; // Assuming the data is in the request body
+  console.log('Received data:', data);
+  io.emit('notification', JSON.stringify(data)); // Emit the notification event
+  res.status(200).send('Notification sent successfully');
+});
+
+const PORT = 1337;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
---
+++ socket_test/client/script.js
@@ -0,0 +1,32 @@
+const socket = io('//localhost:1337');
+
+const input = document.getElementById('socketInput')
+const button = document.getElementById('submit')
+const elementsList = document.getElementById('elementsList')
+
+
+
+socket.on('notification', (data) => {
+    console.log(`New notification: ${data}`);
+    if(elementsList){
+        const node = document.createElement("li");
+        const formattedData = JSON.parse(data)
+        const textnode = document.createTextNode(`Message: ${formattedData.message}, Sender: ${formattedData.sender}`);
+        node.appendChild(textnode);
+        elementsList.appendChild(node);
+    }
+})
+if (button) {
+    button.onclick = () => {
+
+        let socketValue = {
+            name: String(input.value),
+            age: 2
+        }
+
+        socket.emit('message', JSON.stringify(socketValue));
+    }
+}
+
+
+// input.val
